329c714c2f932c247ffe7c8ab6694e419dbadbd0
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-05 20:11:09 +0530

    description_widget.dart: refactored description widget add

---
+++ lib/onboarding screen/widgets/description_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DescriptionWidget extends StatelessWidget {
+  const DescriptionWidget({super.key, required this.description});
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: Text(
+        description,
+        textAlign: TextAlign.start,
+        style: TextStyle(
+          fontFamily: 'Gilroy',
+          color: Colors.black54,
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+}
