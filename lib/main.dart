d83b4653ceb761e0ff0f431e09f473f34229ce30
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-05 18:24:31 +0530

    main.dart: main function and MyApp() add

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      title: 'First Onboarding Screen Design',
+      title: 'First onboarding screen Design',
       home: OnboardingScreen(),
     );
   }
