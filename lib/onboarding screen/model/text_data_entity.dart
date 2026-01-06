1496f4a9350cd5b4ea7b6fabdb926e77b63eca21
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-06 21:14:12 +0530

    text_data_entity.dart: file rename, class rename, constructor rename, and list type is renamed.

--- lib/onboarding screen/model/text_data_entity.dart
+++ lib/onboarding screen/model/text_data_entity.dart
@@ -1,20 +1,20 @@
-class OnBoardingScreenModel {
+class TextDataEntity {
   final String title;
   final String description;
 
-  OnBoardingScreenModel({required this.title, required this.description});
+  TextDataEntity({required this.title, required this.description});
 
   /// List return objects of OnboardingScreenModel data
-  static List<OnBoardingScreenModel> screenData = [
-    OnBoardingScreenModel(
+  static List<TextDataEntity> screenData = [
+    TextDataEntity(
       title: "Explore",
       description: 'Explore your favourite destination around the world.',
     ),
-    OnBoardingScreenModel(
+    TextDataEntity(
       title: "Easy Peasy",
       description: 'Make your travel experience very easy & peasy.',
     ),
-    OnBoardingScreenModel(
+    TextDataEntity(
       title: "Enjoy Tour",
       description: 'Enjoy your favourite destination with your love one.',
     ),
