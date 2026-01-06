f3d471650ebab7ed03b13d7406faf67b8cd0d276
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-06 20:35:47 +0530

    dot_indicator_widget.dart: Row widget, and List.generate() removed.

--- lib/onboarding screen/widgets/dot_indicator_widget.dart
+++ lib/onboarding screen/widgets/dot_indicator_widget.dart
@@ -1,28 +1,20 @@
-import 'package:first_onboarding_screen/onboarding%20screen/model/model.dart';
 import 'package:flutter/material.dart';
 
 class DotIndicatorWidget extends StatelessWidget {
-  DotIndicatorWidget({super.key, required this.isActive});
+  const DotIndicatorWidget({super.key, required this.isActive});
 
   final bool isActive;
-  final List<OnBoardingScreenModel> _onboardingScreenData =
-      OnBoardingScreenModel.screenData;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: List.generate(
-        _onboardingScreenData.length,
-        (index) => AnimatedContainer(
-          duration: Duration(milliseconds: 300),
-          height: 8,
-          width: 8,
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          decoration: BoxDecoration(
-            color: isActive ? Colors.orange : Colors.black12,
-            shape: BoxShape.circle,
-          ),
-        ),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      height: 8,
+      width: 8,
+      margin: EdgeInsets.symmetric(horizontal: 0),
+      decoration: BoxDecoration(
+        color: isActive ? Colors.orange : Colors.black12,
+        shape: BoxShape.circle,
       ),
     );
   }
