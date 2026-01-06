c779b4b81e33f07a50f09041d2835ae3bc0c1a9d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-06 13:44:33 +0530

    background_design_widget.dart: background design widget refactor.

---
+++ lib/onboarding screen/widgets/background_design_widget.dart
@@ -0,0 +1,32 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+
+class BackgroundDesignWidget extends StatelessWidget {
+  const BackgroundDesignWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: DottedBorder(
+        color: Colors.orange.shade100,
+        strokeWidth: 2.5,
+        borderType: BorderType.Circle,
+        dashPattern: [2.5, 8],
+        // dot length, space length
+        child: Container(
+          height: 250,
+          width: 250,
+          margin: EdgeInsets.all(18.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [Color(0xFFF3C6A8), Color(0xFFFFF0E4)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
