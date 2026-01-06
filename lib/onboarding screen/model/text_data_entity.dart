bc6e5161d79b52e16ae48bf6ed84e2c06974bf4c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-06 21:12:21 +0530

    model.dart: file rename

--- lib/onboarding screen/model/text_data_entity.dart
+++ lib/onboarding screen/model/text_data_entity.dart
@@ -1,30 +1,22 @@
 class OnBoardingScreenModel {
   final String title;
   final String description;
-  final String image;
 
-  OnBoardingScreenModel({
-    required this.title,
-    required this.description,
-    required this.image,
-  });
+  OnBoardingScreenModel({required this.title, required this.description});
 
   /// List return objects of OnboardingScreenModel data
   static List<OnBoardingScreenModel> screenData = [
     OnBoardingScreenModel(
       title: "Explore",
       description: 'Explore your favourite destination around the world.',
-      image: 'image',
     ),
     OnBoardingScreenModel(
       title: "Easy Peasy",
       description: 'Make your travel experience very easy & peasy.',
-      image: 'image',
     ),
     OnBoardingScreenModel(
       title: "Enjoy Tour",
       description: 'Enjoy your favourite destination with your love one.',
-      image: 'image',
     ),
   ];
 }
