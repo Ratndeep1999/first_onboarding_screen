f4d75b41d59691dff2d02de4bc9d4b96d7d47640
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-06 15:00:45 +0530

    background_design_widget.dart: center widget remove

--- lib/onboarding screen/widgets/background_design_widget.dart
+++ lib/onboarding screen/widgets/background_design_widget.dart
@@ -6,23 +6,21 @@ class BackgroundDesignWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: DottedBorder(
-        color: Colors.orange.shade100,
-        strokeWidth: 2.5,
-        borderType: BorderType.Circle,
-        dashPattern: [2.5, 5], // dot length, space length
-        child: Container(
-          height: 250,
-          width: 250,
-          margin: EdgeInsets.all(12.0),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: LinearGradient(
-              colors: [Color(0xFFF3C6A8), Color(0xFFFFF0E4)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
+    return DottedBorder(
+      color: Colors.orange.shade100,
+      strokeWidth: 2.5,
+      borderType: BorderType.Circle,
+      dashPattern: [2.5, 5], // dot length, space length
+      child: Container(
+        height: 250,
+        width: 250,
+        margin: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            colors: [Color(0xFFF3C6A8), Color(0xFFFFF0E4)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
         ),
       ),
