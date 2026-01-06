a8d3d9ea98037787b44b44b05e323517db158e74
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-06 15:58:57 +0530

    bottom_section_widget.dart: padding add

---
+++ lib/onboarding screen/widgets/bottom_section_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'dot_indicator_widget.dart';
+import 'icon_button_widget.dart';
+
+class BottomSectionWidget extends StatelessWidget {
+  const BottomSectionWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          /// Previous Button
+          IconButtonWidget(
+            onTap: () {},
+            isPrevious: true,
+            semanticLabel: 'Previous Page',
+          ),
+
+          /// Dot Indicator
+          DotIndicatorWidget(),
+
+          /// Next Button
+          IconButtonWidget(onTap: () {}, semanticLabel: 'Next Page'),
+        ],
+      ),
+    );
+  }
+}
