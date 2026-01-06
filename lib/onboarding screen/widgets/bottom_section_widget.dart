1a66b0ff46e26885da7cc00bf26c7d51af22d957
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-06 20:48:26 +0530

    bottom_section_widget.dart: vertical padding change, List.generate() use inside Row() to show dynamic DotIndicators, length nad pageIndex parameters add to get arguments from Parent page and pass to DotIndicatorWidget child page.

--- lib/onboarding screen/widgets/bottom_section_widget.dart
+++ lib/onboarding screen/widgets/bottom_section_widget.dart
@@ -3,14 +3,19 @@ import 'dot_indicator_widget.dart';
 import 'icon_button_widget.dart';
 
 class BottomSectionWidget extends StatelessWidget {
-  const BottomSectionWidget({super.key, required this.isActive});
+  const BottomSectionWidget({
+    super.key,
+    required this.pageIndex,
+    required this.length,
+  });
 
-  final bool isActive;
+  final int pageIndex;
+  final int length;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -22,7 +27,12 @@ class BottomSectionWidget extends StatelessWidget {
           ),
 
           /// Dot Indicator
-          DotIndicatorWidget(isActive: isActive),
+          Row(
+            children: List.generate(
+              (length),
+              (index) => DotIndicatorWidget(isActive: (index == pageIndex)),
+            ),
+          ),
 
           /// Next Button
           IconButtonWidget(onTap: () {}, semanticLabel: 'Next Page'),
