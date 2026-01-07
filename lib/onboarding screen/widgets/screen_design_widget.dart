2f78dbedcfd26263feaf4b9c1a3f0ce86069160c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-07 13:06:54 +0530

    screen_design_widget.dart: Images path remove and String parameters add

--- lib/onboarding screen/widgets/screen_design_widget.dart
+++ lib/onboarding screen/widgets/screen_design_widget.dart
@@ -2,7 +2,18 @@ import 'package:flutter/material.dart';
 import 'background_design_widget.dart';
 
 class ScreenDesignWidget extends StatelessWidget {
-  const ScreenDesignWidget({super.key});
+  const ScreenDesignWidget({
+    super.key,
+    required this.centerImage,
+    required this.topLeftImage,
+    required this.bottomLeftImage,
+    required this.bottomRightImage,
+  });
+
+  final String centerImage;
+  final String topLeftImage;
+  final String bottomLeftImage;
+  final String bottomRightImage;
 
   @override
   Widget build(BuildContext context) {
@@ -12,38 +23,16 @@ class ScreenDesignWidget extends StatelessWidget {
         Positioned(top: 183, left: 55, child: BackgroundDesignWidget()),
 
         /// Center Image
-        Positioned(
-          top: 80,
-          left: 100,
-          child: Image.asset(
-            'assets/onboarding/images/screen1_center_image.png',
-          ),
-        ),
+        Positioned(top: 80, left: 100, child: Image.asset(centerImage)),
 
         /// Top Left Image
-        Positioned(
-          top: 90,
-          child: Image.asset(
-            'assets/onboarding/images/screen1_top_left_image.png',
-          ),
-        ),
+        Positioned(top: 90, child: Image.asset(topLeftImage)),
 
         /// Bottom Left Image
-        Positioned(
-          top: 300,
-          child: Image.asset(
-            'assets/onboarding/images/screen1_bottom_left_image.png',
-          ),
-        ),
+        Positioned(top: 300, child: Image.asset(bottomLeftImage)),
 
         /// Bottom Right Image
-        Positioned(
-          top: 400,
-          right: 0,
-          child: Image.asset(
-            'assets/onboarding/images/screen1_bottom_right_image.png',
-          ),
-        ),
+        Positioned(top: 400, right: 0, child: Image.asset(bottomRightImage)),
       ],
     );
   }
