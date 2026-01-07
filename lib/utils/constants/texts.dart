2f26bdd5ad9b227eb5d72505b98cc08639b90325
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-07 12:35:16 +0530

    texts.dart: constants onBoarding screen texts add.

---
+++ lib/utils/constants/texts.dart
@@ -0,0 +1,14 @@
+class OnBoardingText {
+  /// Private Constructor
+  OnBoardingText._();
+
+  // OnBoarding Screen1 Title
+  static const String onBoardingScreen1Title = 'Explore';
+  static const String onBoardingScreen2Title = 'Easy Peasy';
+  static const String onBoardingScreen3Title = 'Enjoy Tour';
+
+  // OnBoarding Screen Description
+  static const String onBoardingScreen1Description = 'Explore your favourite destination around the world.';
+  static const String onBoardingScreen2Description = 'Make your travel experience very easy & peasy.';
+  static const String onBoardingScreen3Description = 'Enjoy your favourite destination with your love one.';
+}
