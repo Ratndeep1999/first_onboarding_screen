608a24d3029bbe95a089cc48b992d59ae15633a8
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-07 17:00:17 +0530

    onboarding_screens_model.dart: model add to return onboarding screens.

---
+++ lib/onboarding/model/onboarding_screens_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../screens/onboarding_screen1.dart';
+import '../screens/onboarding_screen2.dart';
+import '../screens/onboarding_screen3.dart';
+
+class OnboardingScreensModel {
+  final Widget screen;
+
+  const OnboardingScreensModel({required this.screen});
+
+  static const List<OnboardingScreensModel> onboardingScreens = [
+    OnboardingScreensModel(screen: OnboardingScreen1()),
+    OnboardingScreensModel(screen: OnboardingScreen2()),
+    OnboardingScreensModel(screen: OnboardingScreen3()),
+  ];
+}
