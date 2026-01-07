530743dbc126493fdb6d07eb1d63db9058ea12e9
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-07 12:54:22 +0530

    images.dart: onboarding screens constant image strings add

---
+++ lib/utils/constants/images.dart
@@ -0,0 +1,34 @@
+class OnBoardingImages {
+  /// Private Constructor
+  OnBoardingImages._();
+
+  // Center Images
+  static const String onBoardingCenterImage1 =
+      'assets/onboarding/images/screen1_center_image.png';
+  static const String onBoardingCenterImage2 =
+      'assets/onboarding/images/screen2_center_image.png';
+  static const String onBoardingCenterImage3 =
+      'assets/onboarding/images/screen3_center_image.png';
+
+  // Top Left Images
+  static const String onBoardingTopLeftImage1 =
+      'assets/onboarding/images/screen1_top_left_image.png';
+  static const String onBoardingTopLeftImage2 =
+      'assets/onboarding/images/screen2_top_left_image.png';
+  static const String onBoardingTopLeftImage3 =
+      'assets/onboarding/images/screen3_top_left_image.png';
+
+  // Bottom Left Images
+  static const String onBoardingBottomLeftImage1 =
+      'assets/onboarding/images/screen1_bottom_left_image.png';
+  static const String onBoardingBottomLeftImage3 =
+      'assets/onboarding/images/screen3_bottom_left_image.png';
+
+  // Bottom Right Images
+  static const String onBoardingBottomRightImage1 =
+      'assets/onboarding/images/screen1_bottom_right_image.png';
+  static const String onBoardingBottomRightImage2 =
+      'assets/onboarding/images/screen2_bottom_right_image.png';
+  static const String onBoardingBottomRightImage3 =
+      'assets/onboarding/images/screen3_bottom_right_image.png';
+}
